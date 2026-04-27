92f89ea5351a5b02963a00e5f1a4f672ff960672
Allen923 <nkhgncha@gmail.com>
2026-04-27 20:56:13 +0800

    優化腸病毒.py和新增圖檔

--- 腸病毒01.py
+++ 腸病毒01.py
@@ -37,7 +37,9 @@ plt.subplot(242)
 s2 = df.groupby("週")["腸病毒健保就診人次"].sum()
 plt.plot(s2.index, s2.values / 10000)
 plt.xlabel("週數", fontsize=16)
-plt.ylabel("腸病毒就診人次 (萬)", fontsize=16)
+plt.ylabel(
+    "腸\n病\n毒\n就\n診\n人\n次\n(萬)", fontsize=16, rotation=0, labelpad=20, y=0.3
+)
 plt.title("每週腸病毒就診人次", fontsize=24)
 for i in s2.index[::5]:  # 每隔5週顯示一次數字
     plt.text(
@@ -58,17 +60,21 @@ plt.text(
     fontsize=10,
     color="red",
 )
+plt.ylim(0, 12)
+plt.grid()
 
 # 3.腸病毒就診類別畫成圓餅圖
 plt.subplot(243)
 s3 = df.groupby("就診類別")["腸病毒健保就診人次"].sum()
+labels = [f"{idx}\n({val:,}人)" for idx, val in s3.items()]
 plt.pie(
     s3.values,
-    labels=s3.index,
+    labels=labels,
     autopct="%10.2f%%",
     startangle=45,
     colors=["#ff9999", "#66b3ff", "#99ff99", "#ffcc99"],
 )
+plt.title("腸病毒就診類別", fontsize=24)
 
 # 4.年齡別腸病毒就診人次畫成長條圖
 plt.subplot(244)
@@ -78,7 +84,9 @@ years = ["0~2", "3~6", "7~12", "13~15", "16~18", "19~24", "25~64", "65+"]
 s4 = s4[years]
 plt.bar(s4.index, s4.values / 10000)
 plt.xlabel("年齡別", fontsize=16)
-plt.ylabel("腸病毒就診人次 (萬)", fontsize=16)
+plt.ylabel(
+    "腸\n病\n毒\n就\n診\n人\n次\n(萬)", fontsize=16, rotation=0, labelpad=20, y=0.3
+)
 plt.title("年齡別腸病毒就診人次", fontsize=24)
 plt.xticks(rotation=45)
 for i in range(len(s4.index)):
@@ -121,18 +129,29 @@ s5 = df.groupby("縣市")["腸病毒健保就診人次"].sum()
 s5 = s5[county_list]
 plt.bar(s5.index, s5.values)
 plt.xlabel("縣市", fontsize=16)
-plt.ylabel("腸病毒就診人次", fontsize=16)
+plt.ylabel("腸\n病\n毒\n就\n診\n人\n次", fontsize=16, rotation=0, labelpad=20, y=0.3)
 plt.title("各縣市腸病毒就診人次", fontsize=24)
 plt.xticks(rotation=45)
 for i in range(len(s5.index)):
-    plt.text(
-        s5.index[i],
-        s5.values[i],
-        f"{s5.values[i]:,}人",
-        ha="center",
-        va="bottom",
-        fontsize=10,
-    )
+    if s5.iloc[i] > 300000:
+        plt.text(
+            s5.index[i],
+            s5.values[i],
+            f"{s5.values[i]:,}人",
+            ha="center",
+            va="bottom",
+            fontsize=10,
+        )
+    else:
+        plt.text(
+            s5.index[i],
+            s5.values[i],
+            f"{s5.values[i]:,}人",
+            ha="center",
+            va="bottom",
+            fontsize=10,
+            rotation=90,
+        )
 
 
 # 6.六都與非六都腸病毒就診人次佔比畫成圓餅圖
@@ -145,13 +164,18 @@ df["六都"] = df["縣市"].apply(
     )
 )
 s6 = df.groupby("六都")["腸病毒健保就診人次"].sum()
-plt.pie(s6.values, labels=s6.index, autopct="%.2f%%")
+labels = [f"{idx}\n({val:,}人)" for idx, val in s6.items()]
+plt.pie(s6.values, labels=labels, autopct="%.2f%%")
+plt.title("六都與非六都腸病毒就診人次佔比", fontsize=24)
 
 
 # 7.各六都腸病毒就診人次比率畫成圓餅圖
 plt.subplot(248)
 s7 = df[df["六都"] == "六都"].groupby("縣市")["腸病毒健保就診人次"].sum()
-plt.pie(s7.values, labels=s7.index, autopct="%.2f%%")
+labels = [f"{idx} \n({val:,})人" for idx, val in s7.items()]
+plt.pie(s7.values, labels=labels, autopct="%.2f%%")
+plt.title("六都腸病毒就診人次比率", fontsize=24)
 
 plt.tight_layout()
+plt.savefig("腸病毒就診統計資料.png")
 plt.show()
